a7997f9dc8089e4cd2ff5f1e4d06f3c190d92b46
Jaw0r3k <jaw0r3k.g@gmail.com>
2024-01-18 22:23:35 +0100

    plans lol

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# jaw0r3k.github.io
- Jaw0r3k site
+Well it is going to be a site about all my current or done projects
+
+Someday i will start fairly documentig them
+
+But as we see it probably wont happen too soon as human is a lazy being
--- utils/index.js
+++
@@ -1,6 +0,0 @@
-theme = window.localStorage.getItem("theme") || (window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light")
-if(theme === "dark"){   
-    document.documentElement.classList.add('dark')
-} else {
-    document.documentElement.classList.remove('dark')
-}
